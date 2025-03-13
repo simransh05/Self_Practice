228ba43d3d3598fd5f181b3f340420b3cdcab5fb
Simran <ss1043422@gmail.com>
2025-03-13 12:34:06 +0530

    LinkedList Implementation

--- re_coding17/LinkedList_Implement.java
+++ re_coding17/LinkedList_Implement.java
@@ -2,13 +2,20 @@ package re_coding17;
 
 public class LinkedList_Implement {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Linked_List ll = new Linked_List();
 		ll.AddFirst(10);
 		ll.AddFirst(20);
 		ll.AddFirst(30);
+		ll.AddLast(40);
+		ll.AddLast(50);
+		ll.AddFirst(12);
+		ll.AddAtIndex(15, 4);
+		ll.AddAtIndex(-10, 2);
 		ll.display();
-
+		System.out.println(ll.RemoveFirst());
+		System.out.println(ll.RemoveLast());
+		System.out.println(ll.RemoveAtIndex(2));
+		ll.display();
 	}
-
 }
--- re_coding17/Linked_List.java
+++ re_coding17/Linked_List.java
@@ -38,12 +38,76 @@ public class Linked_List {
 		}
 	}
 
-	public void AddAtIndex(int val, int k) {
-		node nn = new node();
-		nn.data=val;
-		if(size==0) {
-			
+	public void AddAtIndex(int val, int k) throws Exception {
+		if (k == 0) {
+			AddFirst(val);
+		} else if (k == size) {
+			AddLast(val);
+		} else {
+			node nn = new node();
+			nn.data = val;
+			node k_1th = getNode(k - 1);
+			nn.next = k_1th.next;
+			k_1th.next = nn;
+			size++;
 		}
+	}
+
+	public int GetFirst() {
+		return head.data;
+	}
+
+	public int GetLast() {
+		return tail.data;
+	}
+
+	public int GetAtIndex(int k) throws Exception {
+		return getNode(k).data;
+	}
+
+	public int RemoveFirst() {
+		int rv = head.data;
+		if (size == 1) {
+			head = null;
+			tail = null;
+		} else {
+			node temp = head;
+			head = head.next;
+			temp.next = null;
+		}
+		size--;
+		return rv;
+	}
+
+	public int RemoveLast() throws Exception {
+		int rv = tail.data;
+		if (size == 1) {
+			head = null;
+			tail = null;
+		} else {
+			node secLast = getNode(size - 2);
+			secLast.next = null;
+			tail = secLast;
+		}
+		size--;
+		return rv;
+	}
+
+	public int RemoveAtIndex(int k) throws Exception {
+		int rv = 0;
+		if (k == 0) {
+			return RemoveFirst();
+		} else if (k == size - 1) {
+			return RemoveLast();
+		} else {
+			node kth = getNode(k);
+			node k_1th = getNode(k - 1);
+			rv = kth.data;
+			k_1th.next = kth.next;
+			kth.next = null;
+			size--;
+		}
+		return rv;
 	}
 
 	public void display() {
@@ -52,7 +116,22 @@ public class Linked_List {
 			System.out.print(temp.data + "-->");
 			temp = temp.next;
 		}
-		System.out.println();
+		System.out.println(".");
+	}
+
+	private node getNode(int k) throws Exception {
+		if (k < 0 || k >= size) {
+			throw new Exception("out of range");
+		}
+		node temp = head;
+		for (int i = 0; i < k; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+	public int size() {
+		return size;
 	}
 
 }
